47a3d273440c2592721317defd3fb7db062a6494
Jakob A <jdanders1999@gmail.com>
2023-01-19 18:18:47 -0500

    Set up .get to send routes to index.html

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,2 +1,6 @@
 const path = require("path");
 const router = require('express').Router();
+
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__ddirname, '../../public/index.html'));
+});
