3c5d9f9714bc545b577a55e11f94b8f52e10ab7d
Jakob A <jdanders1999@gmail.com>
2023-01-19 18:25:10 -0500

    Added required path for htmlRouter to index.js and exported

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,9 @@
 const express = require('express');
+const router = require('./htmlRoutes');
+const htmlRouter = require('./htmlRoutes');
 const app = express();
+
+router.use('/htmlRouter', htmlRouter);
+
+module.exports = app;
 
