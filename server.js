017626959f559eeb7e68ddf60a11dab6c7046baf
Jakob A <jdanders1999@gmail.com>
2023-01-19 18:28:14 -0500

    Added path to db.json and initialized express and the port

--- server.js
+++ server.js
@@ -2,3 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 
+let notes = require('./db/db.json');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
