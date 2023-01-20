7c51d14310fbf481beb6cd83930b970cd2669a66
Jakob A <jdanders1999@gmail.com>
2023-01-19 21:05:36 -0500

    Set up .get for index.html, notes.html and aquiring the db.json

--- server.js
+++ server.js
@@ -11,3 +11,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended:true }));
 
 app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+});
